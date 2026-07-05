c4732f69ec2e25711db5bfeabfeb98662ea5ca4e
Rishi Gupta <cellrishi@gmail.com>
2026-07-05 10:37:24 +0545

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,26 @@
+# how to remove the duplicate items from the list using the set
+
+# lst = [8,32,42,323,4545,244,8,8]
+# unique = list(set(lst))
+
+# print(unique)
+
+
+## elements in the set only contains immuatble dataypes like tuple not list or disnory
+
+##string is imutable data str in py
+
+# for ch in "hello":
+#     print(ch)
+    
+    
+## "is" keyword in py is used to compare the memory location in python
+
+
+
+a = 4387847349374827349873979248789442
+b = 4387847349374827349873979248789442
+
+print(a is b)       # answer will be true here 
+
+#Python’s optimization called integer interning. due this , this happens 
